0614d4fb777a1043e2b3fdb655ea64cb35862c20
Bruno Maia <111490358+brunomaiaa@users.noreply.github.com>
2023-01-31 17:22:42 +0100

    done
    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import "./css/style.css"; //importing css
+
+const displayModal = () => {
+    // Get the modal
+    const modal = document.getElementById("myModal");
+  
+    // Show the modal
+    modal.style.display = "block";
+  
+    // Get the close button
+    const closeBtn = document.querySelector(".close");
+  
+    // Close the modal when the close button is clicked
+    closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+  
+    // Close the modal when the user clicks outside of it
+    window.addEventListener("click", (event) => {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  };
+  
+  const delay = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 60000);
+  });
+  
+  delay.then(displayModal);
+  
